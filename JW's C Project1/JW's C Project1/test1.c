8ef6a91b25ca99ea0d4a0762ad463e2d4e83ab01
Voliti1 <sjwnick2010@gmail.com>
2026-03-17 16:06:48 +0900

    C Project

--- JW's C Project1/JW's C Project1/test1.c
+++ JW's C Project1/JW's C Project1/test1.c
@@ -1,6 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-	int i = 10;
-	printf("%d", i);
+	int age;
+	printf("age?\n");
+	//scanf("%d", &age);
+
+	printf("age : %d", &age);
 }
